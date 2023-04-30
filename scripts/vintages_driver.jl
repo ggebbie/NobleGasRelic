0acc94cd381ed11479c15f4967898bd991b515e9
G Jake Gebbie <ggebbie@whoi.edu>
2023-04-30 09:51:10 -0400

    next add inversion with BLUEs

--- scripts/vintages_driver.jl
+++ scripts/vintages_driver.jl
@@ -3,14 +3,24 @@
 Gives a sense of the order of each script
 =#
 
-## Do numerical calculations for global distribution of vintages
-#include(scriptsdir("vintages_calculate.jl"))
+#=
+Numerical calculations section
+
+=#
+
+# calculations for global distribution of vintages
+include(scriptsdir("vintages_calculate.jl"))
 
 # Gives vintage information needed for inversion
 # calls vintages_calculate if necessary
 include(scriptsdir("vintages_table_NPACvSPAC.jl"))
 
-## Diagostics
+## HERE: add inversion routines
+
+
+#=
+Diagostics Section
+=#
 
 # make planview figures of vintages
 include(scriptsdir("vintages_planviews.jl"))
