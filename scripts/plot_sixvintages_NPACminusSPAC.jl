6ebd4a233e64996e17cf183f26278371c879be31
G Jake Gebbie <ggebbie@whoi.edu>
2023-05-02 15:37:47 -0400

    new file: plot timeseries

---
+++ scripts/plot_sixvintages_NPACminusSPAC.jl
@@ -0,0 +1,40 @@
+#using DrWatson
+#@quickactivate "NobleGasRelic"
+
+using Revise
+using NobleGasRelic
+using DrWatson
+using DataFrames
+using CSV
+using BLUEs
+using Unitful
+using UnitfulLinearAlgebra
+using Plots
+
+include(srcdir("config_vintages.jl"));
+
+# should actually be computed from CSV file
+t̄ = midtime(tinterval)
+
+# Solve three cases.
+cases = ("min_trend","min_variance","min_trend_variance")
+for case in cases
+
+    csvfile = datadir("sixvintages_"*case*".csv")
+
+    # Do computations if necessary
+    !isfile(csvinput) && include(scriptsdir("invert_sixvintages_NPACminusSPAC.jl"))
+
+    df = DataFrame(CSV.File(csvfile))
+    # make a plot
+
+    if case == "min_trend"
+        # should read units from CSV
+        plot(ustrip.(collect(values(t̄))),ustrip.(df[:,6]mbar),ribbon=ustrip.(df[:,7]mbar),label=case,xlabel="calendar years",ylabel="SLP anomaly")
+    else
+        plot!(collect(values(t̄)),df[:,6],ribbon=df[:,7],label=case,xlabel="years [CE]",ylabel="SLP anomaly",xticks=(-450:250:2022))
+        
+    end
+    savefig(plotsdir("SLP_CommonEra_combined.pdf"))
+end
+test
